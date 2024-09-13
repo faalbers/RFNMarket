5f2daccfb53c256e256c1abc55eb63a18e3e5fde
faalbers <chalbers@gmail.com>
2024-09-12 19:26:11 -0700

    fixed recursing import

--- rfnmarket/tickers.py
+++ rfnmarket/tickers.py
@@ -1,4 +1,4 @@
-from . import database, scrape
+from . import scrape
 from .utils import log
 from pprint import pp
 
@@ -33,76 +33,76 @@ class Tickers():
                 scraperObjects[scraperClass] = scraperClass(symbols, types)
 
 
-class TickersOld():
-    def __init__(self, logLevel=log.WARNING):
-        log.initLogger(logLevel=logLevel)
-        self.dbEntries = set()
+# class TickersOld():
+#     def __init__(self, logLevel=log.WARNING):
+#         log.initLogger(logLevel=logLevel)
+#         self.dbEntries = set()
         
-    @property
-    def dividends(self):
-        self.dbEntries.add((
-            database.TimeSeries,
-            database.TimeSeries.getDividends,
-            'dividends'))
+#     @property
+#     def dividends(self):
+#         self.dbEntries.add((
+#             database.TimeSeries,
+#             database.TimeSeries.getDividends,
+#             'dividends'))
 
-    @property
-    def splits(self):
-        self.dbEntries.add((
-            database.TimeSeries,
-            database.TimeSeries.getSplits,
-            'splits'))
+#     @property
+#     def splits(self):
+#         self.dbEntries.add((
+#             database.TimeSeries,
+#             database.TimeSeries.getSplits,
+#             'splits'))
     
-    @property
-    def prices(self):
-        self.dbEntries.add((
-            database.TimeSeries, {},
-            database.TimeSeries.getPrices,
-            'prices'))
+#     @property
+#     def prices(self):
+#         self.dbEntries.add((
+#             database.TimeSeries, {},
+#             database.TimeSeries.getPrices,
+#             'prices'))
     
-    @property
-    def volumes(self):
-        self.dbEntries.add((
-            database.TimeSeries,
-            database.TimeSeries.getVolumes,
-            'volumes'))
+#     @property
+#     def volumes(self):
+#         self.dbEntries.add((
+#             database.TimeSeries,
+#             database.TimeSeries.getVolumes,
+#             'volumes'))
     
-    @property
-    def splits(self):
-        self.dbEntries.add((
-            database.TimeSeries,
-            database.TimeSeries.getSplits,
-            'splits'))
+#     @property
+#     def splits(self):
+#         self.dbEntries.add((
+#             database.TimeSeries,
+#             database.TimeSeries.getSplits,
+#             'splits'))
 
-    @property
-    def profile(self):
-        self.dbEntries.add((
-            database.Summary,
-            database.Summary.getProfiles,
-            'profile'))
+#     @property
+#     def profile(self):
+#         self.dbEntries.add((
+#             database.Summary,
+#             database.Summary.getProfiles,
+#             'profile'))
     
-    @property
-    def statistics(self):
-        self.dbEntries.add((
-            database.Summary,
-            database.Summary.getStatistics,
-            'statistics'))
+#     @property
+#     def statistics(self):
+#         self.dbEntries.add((
+#             database.Summary,
+#             database.Summary.getStatistics,
+#             'statistics'))
 
-    def getData(self, symbols, update=False, updateMax=False):
-        dbObjects = {}
-        # creta database objects
-        for dbClass, dbCommand, entryName in self.dbEntries:
-            if not dbClass in dbObjects:
-                dbObjects[dbClass] = dbClass()
-        # update databases
-        if update or updateMax:
-            for dbClass, dbObject in  dbObjects.items():
-                dbObject.update(symbols, updateMax=updateMax)
-        # retieve data from databases
-        # do not return data when running an updateMax
-        if updateMax: return {}
-        data = {}
-        for dbClass, dbCommand, entryName in self.dbEntries:
-            data[entryName] = dbCommand(dbObjects, symbols)
-        return data
+#     def getData(self, symbols, update=False, updateMax=False):
+#         dbObjects = {}
+#         # creta database objects
+#         for dbClass, dbCommand, entryName in self.dbEntries:
+#             if not dbClass in dbObjects:
+#                 dbObjects[dbClass] = dbClass()
+#         # update databases
+#         if update or updateMax:
+#             for dbClass, dbObject in  dbObjects.items():
+#                 dbObject.update(symbols, updateMax=updateMax)
+#         # retieve data from databases
+#         # do not return data when running an updateMax
+#         if updateMax: return {}
+#         data = {}
+#         for dbClass, dbCommand, entryName in self.dbEntries:
+#             data[entryName] = dbCommand(dbObjects, symbols)
+#         return data
 
 
