ecbeb0a62225b58431ab11879c2f778f57c38c46
faalbers <chalbers@gmail.com>
2024-10-08 19:29:23 -0700

    updated with latest database

--- quickenCSVImport.py
+++ quickenCSVImport.py
@@ -8,13 +8,8 @@ if __name__ == '__main__':
     data = tickers.getQuickenInvestments()
     symbols = list((data.keys()))
     symbols.sort()
-    # data = tickers.getData(symbols, ['price'], forceUpdate=True)
     data = tickers.getData(['price'], symbols, forceUpdate=True)
-    dftn = data['price']['price']['price']['price']
-    
-    # dftn['timestamp'] = pd.to_datetime(dftn['timestamp'], unit='s').dt.tz_localize('US/Pacific')
-    # dftn['marketTime'] = pd.to_datetime(dftn['marketTime'], unit='s').dt.tz_localize('US/Pacific')
-
-    dftn.dropna().to_csv('Z:\\Quicken\\QuickenImport.csv', sep=',', encoding='utf-8')
+    dftn = pd.DataFrame(data['price']).T['price'].dropna()
+    dftn.to_csv('Z:\\Quicken\\QuickenImport.csv', header=False, sep=',', encoding='utf-8')
 
 
